99118d57ba2828117a8c4372db970f97121d18eb
ychalodiya <ychalodiya@gmail.com>
2023-10-21 22:21:54 -0400

    setup netflix and player pages as well as navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Netflix from './pages/Netflix';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
+import Player from './pages/Player.jsx';
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route exact path="/" element={<Netflix />} />
 				<Route exact path="/login" element={<Login />} />
+				<Route exact path="/player" element={<Player />} />
 				<Route exact path="/signup" element={<Signup />} />
 			</Routes>
 		</BrowserRouter>
--- client/src/pages/Netflix.jsx
+++ client/src/pages/Netflix.jsx
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import styled from 'styled-components';
+import background from '../assets/home.jpg';
+import MovieLogo from '../assets/homeTitle.webp';
+import { FaPlay } from 'react-icons/fa';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
-const Netflix = () => {
-	return <div>Netflix</div>;
-};
+export default function Netflix() {
+	const navigate = useNavigate();
+	const [isScrolled, setIsScrolled] = useState(false);
+	window.onscroll = () => {
+		setIsScrolled(window.scrollY === 0 ? false : true);
+		return () => (window.onscroll = null);
+	};
+	return (
+		<Container>
+			<Navbar isScrolled={isScrolled} />
+			<div className="hero">
+				<img src={background} alt="background" className="background-image" />
+				<div className="container">
+					<div className="logo">
+						<img src={MovieLogo} alt="Movie Logo" />
+					</div>
+					<div className="buttons flex">
+						<button
+							className="flex a-center j-center"
+							onClick={() => navigate('/player')}
+						>
+							<FaPlay /> Play
+						</button>
+						<button className="flex a-center j-center">
+							<AiOutlineInfoCircle /> More Info
+						</button>
+					</div>
+				</div>
+			</div>
+		</Container>
+	);
+}
 
-export default Netflix;
+const Container = styled.div`
+	background-color: black;
+	.hero {
+		position: relative;
+		.background-image {
+			filter: brightness(60%);
+		}
+		img {
+			height: 100vh;
+			width: 100vw;
+		}
+		.container {
+			position: absolute;
+			bottom: 5rem;
+			.logo {
+				img {
+					width: 100%;
+					height: 100%;
+					margin-left: 5rem;
+				}
+			}
+			.buttons {
+				margin: 5rem;
+				gap: 2rem;
+				button {
+					font-size: 1.4rem;
+					gap: 1rem;
+					border-radius: 0.2rem;
+					padding: 0.5rem;
+					padding-left: 2rem;
+					padding-right: 2.4rem;
+					border: none;
+					cursor: pointer;
+					transition: 0.2s ease-in-out;
+					&:hover {
+						opacity: 0.8;
+					}
+					&:nth-of-type(2) {
+						background-color: rgba(109, 109, 110, 0.7);
+						color: white;
+						svg {
+							font-size: 1.8rem;
+						}
+					}
+				}
+			}
+		}
+	}
+`;
