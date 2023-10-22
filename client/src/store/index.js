73f125dc65769983452e6b8fc4b3b377f181238a
ychalodiya <ychalodiya@gmail.com>
2023-10-21 22:24:54 -0400

    initializing redux store

---
+++ client/src/store/index.js
@@ -0,0 +1,19 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	movies: [],
+	genresLoaded: false,
+	genres: [],
+};
+
+const NetflixSlice = createSlice({
+	name: 'Netflix',
+	initialState,
+	extraReducers: (builder) => {},
+});
+
+export const store = configureStore({
+	reducer: {
+		netflix: NetflixSlice.reducer,
+	},
+});
